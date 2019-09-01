546ee56fd6ee0328b7d6c02b9dde0ed9f42104c9
Fernando Dorantes <fernando.donantes@gigigo.com.mx>
2019-09-01 13:17:35 -0500

    Replace http for https

--- services/ProductService.js
+++ services/ProductService.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export default class ProductService {
   static getProducts () {
-    return axios.get(`http://api-store.dorant.es/products`)
+    return axios.get(`https://api-store.dorant.es/products`)
   }
   static getProduct (id) {
-    return axios.get(`http://api-store.dorant.es/products/${id}`)
+    return axios.get(`https://api-store.dorant.es/products/${id}`)
   }
 }
