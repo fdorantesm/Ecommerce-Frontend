a78fd30f26fa97f6ea0d3a0ea76cd2dfd5ba6583
Fernando Dorantes <fernando.donantes@gigigo.com.mx>
2019-09-07 12:39:26 -0500

    Replace href for to in header

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,47 +1,18 @@
 <template>
   <header id="header">
-    <!-- <div class="container dfx aic">
-      <nuxt-link to="/">
-        <img src="/img/logo.png">
-      </nuxt-link>
-      <nav class="nav">
-        <ul class="nav__list">
-          <li class="nav__item">
-            <nuxt-link to="/products">Products</nuxt-link>
-          </li>
-          <li class="nav__item">
-            <nuxt-link class="nav__link" to="/auth">Login</nuxt-link>
-          </li>
-          <li class="nav__item">
-            <nuxt-link class="nav__link" to="/cart">Cart</nuxt-link>
-          </li>
-          <li class="nav__item">
-            <nuxt-link class="nav__link" to="/">Item</nuxt-link>
-          </li>
-        </ul>
-      </nav>
-    </div> -->
     <div>
       <b-navbar toggleable="lg">
-        <b-navbar-brand href="#">
+        <b-navbar-brand to="/">
           <nuxt-link to="/">eCommerce</nuxt-link>
         </b-navbar-brand>
-
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item><nuxt-link to="/">Home</nuxt-link></b-nav-item>
             <b-nav-item><nuxt-link to ="/products">Products</nuxt-link></b-nav-item>
           </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <!-- <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-            </b-nav-item-dropdown> -->
-            <b-nav-item href="#">
+            <b-nav-item to="#">
               <nuxt-link to="/cart">
                 <fa icon="shopping-cart"/>
               </nuxt-link>
@@ -51,7 +22,7 @@
             </b-nav-item>
             <b-nav-item-dropdown v-if="auth.user._id" right>
               <template slot="button-content"><fa icon="user"/> <em>{{auth.user.profile.firstName}}</em></template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item to="#">Profile</b-dropdown-item>
               <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
           </b-navbar-nav>
